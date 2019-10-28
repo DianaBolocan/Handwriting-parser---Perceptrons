c8ced1c66b24d028934a54a388ea14adb2623c73
Diana Bolocan <diana.bolocan@arcadis.com>
2019-10-28 15:59:34 +0200

    update backpropagation

--- backpropagation.py
+++ backpropagation.py
@@ -1,70 +1,52 @@
 import numpy as np, _pickle as cPickle, gzip
 
 
-# noIterations = 10
-# rate = 0.01
-#
-# weights = np.random.rand((784, 10))
-# biases = np.random.rand((1, 784))
-#
-# # n iterari pe fiecare perceptron
-# for j in range(noIterations):
-#     result = []
-#     newWeights = []
-#     newBiases = []
-#
-#     print("Iteration:", j)
-#     for i in range(10):
-#         result = perceptronTraining(i, train_set, weights[i], biases[i], rate)
-#         newWeights.append(result[0])
-#         newBiases.append(result[1])
-#
-#     newWeights = np.array(newWeights)
-#     newBiases = np.array(newBiases)
-#     biases = newBiases
-#     weights = newWeights
-#
-#     # do the test
-#     digits = []
-#     for input in test_set[0]:
-#         max = 0
-#         digit = 0
-#         for perc in range(10):
-#             compute = perceptronCompute(perc, input, newWeights[perc], newBiases[perc])
-#             if max < compute:
-#                 max = compute
-#                 digit = perc
-#         digits.append(digit)
-#
-#     match = 0
-#     for i in range(len(digits)):
-#         if digits[i] == test_set[1][i]:
-#             match += 1
-#
-#     print(match / len(digits) * 100)
-#
-#     # shuffle the train_set
-#     indexes = []
-#     train = []
-#     results = []
-#     for index in range(len(train_set[0])):
-#         indexes.append(index)
-#     random.shuffle(indexes)
-#     for i in indexes:
-#         train.append(train_set[0][i])
-#         results.append(train_set[1][i])
-#     train = np.array(train)
-#     results = np.array(results)
-#     train_set = [train, results]
+class DNN:
+    class __InputLayer:
+        def __init__(self, input_size: int, output_size: int):
+            self.weights = np.random.random((input_size, output_size))
+
+        def total_net(self, data: np.array):
+            return self.weights.dot(data)
+
+    class __HiddenLayer:
+        def __init__(self, input_size: int, output_size: int):
+            self.weights = np.random.random((input_size, output_size))
+            self.biases = np.random.random((input_size, 1))
+
+        def total_net(self, data: np.array):
+            return self.weights.dot(data) + self.biases
+
+        def sigmoid(self, x: float):
+            return 1 / (1 + np.exp(x))
+
+    class __OutputLayer:
+        def __init__(self, input_size: int):
+            self.biases = np.random.random((input_size, 1))
+
+        def softmax(self, x: float):
+            return np.exp(x) / np.sum(np.exp(x))
+
+        def cross_entropy(self, results: np.array, labels: np.array):
+            return results - labels
+
+    def __init__(self, sizes: list, learning_rate: float):
+        if len(sizes) != 3 or type(sizes[0]) != int or type(sizes[1]) != int or type(sizes[2]) != int:
+            raise Exception("Expecting 3 int values got {} values: {}.".format(len(sizes), sizes))
+        self.input = self.__InputLayer(sizes[0], sizes[1])
+        self.hidden = self.__HiddenLayer(sizes[1], sizes[2])
+        self.output = self.__OutputLayer(sizes[2])
+        self.learning_rate = learning_rate
+
+    def train(self):
+        return
+
+    def predict(self):
+        return
 
 
-def initialize_model(number_of_perceptrons: int, weights_size: int, biases_size: int):
-    weights = np.random.random((number_of_perceptrons, weights_size))
-    biases = np.random.random((biases_size, 1))
-    return weights, biases
-
-
-def train(number_of_iterations: int, learning_rate: float, weights: np.array, biases: np.array, train_data, batch_size: int):
+def train(number_of_iterations: int, learning_rate: float, weights: np.array, biases: np.array, train_data,
+          batch_size: int):
     delta = np.zeros((10, 784))
     beta = np.zeros((10, 1))
     while number_of_iterations:
@@ -106,9 +88,11 @@ if __name__ == '__main__':
     train_set, valid_set, test_set = cPickle.load(f, encoding='latin1')
     f.close()
 
-    weights, biases = initialize_model(10, 784, 10)
-    weights, biases = train(20, 0.05, weights, biases, train_set, 5000)
-    predict(train_set, weights, biases)
-    results = predict(test_set, weights, biases)
-    print(results)
-    print(test_set[1])
+    # weights, biases = initialize_model(10, 784, 10)
+    # weights, biases = train(20, 0.05, weights, biases, train_set, 5000)
+    # predict(train_set, weights, biases)
+    # results = predict(test_set, weights, biases)
+    # print(results)
+    # print(test_set[1])
+
+    DNN([784, 100, 10], 1)
